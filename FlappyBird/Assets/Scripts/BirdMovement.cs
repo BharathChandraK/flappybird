01ee5a891256e450a0abea1cbc489fb7719d0027
Chutiya <42772243+bharath@users.noreply.github.com>
2019-04-01 23:56:55 +0530

    flappybird

---
+++ FlappyBird/Assets/Scripts/BirdMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BirdMovement : MonoBehaviour
+{
+    Rigidbody rd;
+
+    int score = 0;
+
+    public Text scoreText;
+
+    private void Start()
+    {
+        rd = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rd.AddForce(0, 6, 0, ForceMode.Impulse);
+            Debug.Log(score);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obsticle")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "score")
+        {
+            score++;
+            scoreText.text = " Score :" + score.ToString();
+            Debug.Log(score);
+        }
+    }
+
+}
---
+++ FlappyBird/Assets/Scripts/Obsticle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obsticle : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime);
+        if(transform.position.x<-15f)
+        {
+            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+        }
+    }
+}
